1b0f860236c1caa2b4c7b91492e8cc5bc375a725
shravansukumar <shravan.sukumar@gmail.com>
2018-02-20 00:44:55 +0530

    sockets implemented

--- stocks-cake/CakeLandingViewController.swift
+++ stocks-cake/CakeLandingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CakeLandingViewController.swift
 //  stocks-cake
 //
 //  Created by Shravan Sukumar on 17/02/18.
@@ -9,10 +9,11 @@
 import UIKit
 import SocketIO
 
-class ViewController: UIViewController {
+class CakeLandingViewController: UIViewController {
     
     var manager: SocketManager!
     var socket: SocketIOClient!
+    var csvData = [[Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,18 +21,40 @@ class ViewController: UIViewController {
         setupSockets()
     }
     
+    private func parse(data: String) {
+        var rowData = [Any]()
+        
+        
+        
+        
+        
+        let rows = data.components(separatedBy: ",")
+        csvData.append(rows)
+        print(csvData)
+    }
+    
     private func setupSockets() {
         manager = SocketManager(socketURL: URL(string: "http://kaboom.rksv.net/")!, config: [.log(true), .compress])
         
-        socket = SocketIOClient(manager: manager, nsp: "/watch")//manager.defaultSocket
+        socket = manager.socket(forNamespace: "/watch")
         
         socket.onAny {print("Got event: \($0.event), with items: \(String(describing: $0.items))")}
         
         socket.on("data", callback: { (data, ack) in
             print(data)
-
-
+            print(ack)
+            
+            if let data = data as? [Any] {
+                if let firstData = data.first as? String {
+                    self.parse(data: firstData)
+                }
+            }
+            
+            ack.with(1)
+            
         })
+        
+        
         
         socket.on("error", callback: { (data, ack) in
             print(data)
@@ -39,17 +62,24 @@ class ViewController: UIViewController {
             
         })
         
+        socket.on(clientEvent: .disconnect) { (data, ack) in
+            print(data)
+            print(ack)
+            
+        }
+        
         
         socket.on("connect", callback: { (data, ack) in
             print(data)
             print(ack)
 
 
-            self.socket.emit("ping", [])
+         //   self.socket.emit("ping", [])
+            self.socket.emit("sub", ["state" : true])
 
         })
-
         socket.connect()
+        
     }
     
 }
